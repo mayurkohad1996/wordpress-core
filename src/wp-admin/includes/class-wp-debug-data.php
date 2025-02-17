790bac8e6c03f673f0a93f4ef998057cfe0f4b58
Sainath Poojary <spoojary614@gmail.com>
2025-02-17 20:06:34 +0530

    Site Health: Fix incorrect rename of $is_fonts_dir_exist to $fonts_dir_exists

--- src/wp-admin/includes/class-wp-debug-data.php
+++ src/wp-admin/includes/class-wp-debug-data.php
@@ -1646,13 +1646,13 @@ class WP_Debug_Data {
 	 */
 	private static function get_wp_filesystem(): array {
 		$upload_dir                     = wp_upload_dir();
+		$fonts_dir_exists               = file_exists( wp_get_font_dir()['basedir'] );
 		$is_writable_abspath            = wp_is_writable( ABSPATH );
 		$is_writable_wp_content_dir     = wp_is_writable( WP_CONTENT_DIR );
 		$is_writable_upload_dir         = wp_is_writable( $upload_dir['basedir'] );
 		$is_writable_wp_plugin_dir      = wp_is_writable( WP_PLUGIN_DIR );
 		$is_writable_template_directory = wp_is_writable( get_theme_root( get_template() ) );
-		$is_fonts_dir_exist             = file_exists( wp_get_font_dir()['basedir'] );
-		$fonts_dir_exists               = $is_fonts_dir_exist ? wp_is_writable( wp_get_font_dir()['basedir'] ) : false;
+		$is_writable_fonts_dir          = $fonts_dir_exists ? wp_is_writable( wp_get_font_dir()['basedir'] ) : false;
 
 		$fields = array(
 			'wordpress'  => array(
@@ -1682,11 +1682,11 @@ class WP_Debug_Data {
 			),
 			'fonts'      => array(
 				'label' => __( 'The fonts directory' ),
-				'value' => $is_fonts_dir_exist
-					? ( $fonts_dir_exists ? __( 'Writable' ) : __( 'Not writable' ) )
+				'value' => $fonts_dir_exists
+					? ( $is_writable_fonts_dir ? __( 'Writable' ) : __( 'Not writable' ) )
 					: __( 'Does not exist' ),
-				'debug' => $is_fonts_dir_exist
-					? ( $fonts_dir_exists ? 'writable' : 'not writable' )
+				'debug' => $fonts_dir_exists
+					? ( $is_writable_fonts_dir ? 'writable' : 'not writable' )
 					: 'does not exist',
 			),
 		);
